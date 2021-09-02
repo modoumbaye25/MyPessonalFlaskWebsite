ef3d10e6208572af88c355a9f0261e26e9f65e54
modoumbaye25 <modoumbaye25@gmail.com>
2021-09-02 09:10:22 -0700

    font fixes, bug fixes, codedeploy pipeline fixes

--- app.py
+++ app.py
@@ -7,27 +7,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():  # put application's code here
+def home():
     year = datetime.datetime.now().year
     return render_template("index.html", current_year=year)
 
 
 @app.route("/projects")
 def projects():
-    random_number = random.randint(1, 10)
-    return render_template('projects.html', num=random_number)
+    return render_template('projects.html')
+
 
 @app.route("/games")
 def games():
     return render_template('games.html')
+
 
 @app.route("/About")
 def about():
     return render_template('about.html')
 
 
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
