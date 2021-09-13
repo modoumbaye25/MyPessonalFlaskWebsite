e5e78d9e11b2023057ce7580ea601d7d6a6b1787
modoumbaye25 <modoumbaye25@gmail.com>
2021-09-13 11:41:31 -0700

    styling changes, fix typos

--- app.py
+++ app.py
@@ -5,21 +5,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+year = datetime.datetime.now().year
+
 
 @app.route('/')
 def home():
-    year = datetime.datetime.now().year
     return render_template("index.html", current_year=year)
 
 
 @app.route("/projects")
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', current_year=year)
 
 
 @app.route("/About")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', current_year=year)
 
 
 if __name__ == '__main__':
