9da7ff0b4202a5d7e3dc9c3a3404f77bbd4c5d8f
Modou Mbaye <modoumbaye25@gmail.com>
2024-02-19 00:56:27 -0800

    website update, add resume

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def home():
     return render_template("index.html", current_year=year)
 
 
-@app.route("/projects")
+@app.route("/Projects")
 def projects():
     return render_template('projects.html', current_year=year)
 
 
-@app.route("/About")
-def about():
-    return render_template('about.html', current_year=year)
+@app.route("/Resume")
+def resume():
+    return render_template('resume.html', current_year=year)
 
 
 if __name__ == '__main__':
